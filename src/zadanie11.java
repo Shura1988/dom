a82cba17781bbf62bd79f760965221b04749bcd8
Aleksandr Kozlou <shura_88_88@mail.ru>
2019-09-05 08:26:13 +0300

    zadanie 11

---
+++ src/zadanie11.java
@@ -0,0 +1,23 @@
+/*Имеется целое число, определить является ли это число простым, т.е.
+        делится без остатка только на 1 и себя.*/
+import java.util.Scanner;
+public class zadanie11 {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter a four digit number : ");
+        int num = in.nextInt();
+        for (int i = 2; i <= num; i++) {
+            if (num%Math.sqrt(num)==0){
+                System.out.println("Число : " + num + "  Составное");
+                break;}
+
+                 else{
+                     System.out.println("Число : " + num + "  Простое");
+                 break;
+        }}
+            in.close();
+
+
+    }
+}
+
