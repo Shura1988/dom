b738c59d08fcd4855c7d15c66f771c789c09c4ee
Aleksandr Kozlou <shura_88_88@mail.ru>
2019-09-05 08:49:25 +0300

    zadanie 11 s doraborkoy

--- src/zadanie11.java
+++ src/zadanie11.java
@@ -7,7 +7,7 @@ public class zadanie11 {
         System.out.println("enter a four digit number : ");
         int num = in.nextInt();
         for (int i = 2; i <= num; i++) {
-            if (num%Math.sqrt(num)==0){
+            if (num%Math.sqrt(num)==0 || num%2==0){
                 System.out.println("Число : " + num + "  Составное");
                 break;}
 
