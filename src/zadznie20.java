b29083f5318295f71ccb74dc063b1b16cda76aa1
Aleksandr Kozlou <shura_88_88@mail.ru>
2019-09-13 11:07:37 +0300

    met2zadanie 20

---
+++ src/zadznie20.java
@@ -0,0 +1,18 @@
+
+import java.lang.*;
+import java.util.regex.Pattern;
+
+public class zadznie20 {
+    public static void main(String[] args) {
+        String str = "Программистами не рождаются, програмистами становятся! ";
+        String[] slova  = str.split("[\\s[,;.:!]]+");
+        String bykvi = "";
+
+        for (int i = 0; i < slova.length; i++) {
+            bykvi += slova[i].substring(slova[i].length()-1 );
+        }
+
+        System.err.println(str);
+        System.out.println(bykvi);
+}
+}
