8bfbfa49869a85cacb45b2af5965b9ea4633a70e
Aleksandr Kozlou <shura_88_88@mail.ru>
2019-09-29 15:12:54 +0300

    metodichka 3 zadanie 33!

--- src/Met3Zadanie33.java
+++ src/Met3Zadanie33.java
@@ -6,13 +6,14 @@
 
 import java.io.*;
 import java.util.Scanner;
+import java.lang.String;
+
 
 public class Met3Zadanie33 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         try {
-        BufferedInputStream txt = new BufferedInputStream(new FileInputStream ("new.txt"));
-
+            BufferedInputStream txt = new BufferedInputStream(new FileInputStream("new.txt"));
             System.out.println(" Исходный текст: ");
             int res = txt.read();
             while (res != -1) {
@@ -20,7 +21,6 @@ public class Met3Zadanie33 {
                 res = txt.read();
             }
             System.out.println();
-
             File text = new File("new.txt");
             Scanner sc = new Scanner(new FileInputStream(text));
             int count = 0;
@@ -29,26 +29,37 @@ public class Met3Zadanie33 {
                 count++;
             }
             System.out.println("Колличество слов в тексте: " + count);
-
-            int count2 = 0;
-            while (sc.hasNext("[.,!;:?]+")) {
-                sc.next(" ");
-                count2++;
-            }
-                System.out.println("Колличество знаков препинания: " + count2);
-
-               // System.out.println("нет знаков препинания");
-
-
-            sc.close();
             txt.close();
+            sc.close();
+
+            FileReader myFile = null;
+            BufferedReader buff = null;
+            String line = new String();
+            try {
+                myFile = new FileReader("new.txt");
+                buff = new BufferedReader(myFile);
+                int count2 = 0;
+                while ((line = buff.readLine()) != null) {
+
+                    char[] m = line.toCharArray();
+                    for (int i = 0; i < m.length; i++) {
+                        if (m[i] == ',' || m[i] == '.' || m[i] == '!' || m[i] == ';') {
+                            count2++;
+                        }
+                    }
+                }
+                System.out.println("Колличество знаков препинания : " + count2);
+
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+
         } catch (IOException e) {
             System.out.println(e);
         }
-
-
     }
 }
+
 
 
 
