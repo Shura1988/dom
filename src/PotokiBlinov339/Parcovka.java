1c89460459a6193330b5245f3f9abdeb689aadee
Aleksandr Kozlou <shura_88_88@mail.ru>
2019-10-06 09:07:26 +0300

    potokiBlinov 339 semafor, predidyschaya versiya zacomrnchena

--- src/PotokiBlinov339/Parcovka.java
+++ src/PotokiBlinov339/Parcovka.java
@@ -4,8 +4,73 @@ package PotokiBlinov339;
         биль не станет ждать больше определенного времени и уедет на другую
         стоянку.*/
 
+import java.util.concurrent.Semaphore;
+
 public class Parcovka extends Thread {
-    private String nameTread;
+    private static final Semaphore parking = new Semaphore(5, true);//парктинг с 5 свободными местами
+    private String car;
+    long time = (long) (Math.random() * 2000);
+
+    public Parcovka(String car) {
+        this.car = car;//название авто
+    }
+
+    @Override
+    public void run() {
+            checkparking();
+            try {
+                System.out.println(" Car " + car + " Waiting");
+                Thread.sleep(time);
+                checkparking();
+                System.out.println("Car " + car + " is go in free parking ");
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
+
+        }
+
+    private void checkparking() {
+            try {
+                parking.acquire();
+                System.out.println("  Car  " + car + "   Stand in parcingpoint ");
+                Thread.sleep(time*2);
+                System.out.println(" Car " + car + "  Exit in  parkingpoint ");
+                parking.release();
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+
+    }
+
+
+    public static Semaphore getParking() {
+        return parking;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* private String nameTread;
     private static int parcingpoin = 3;
     int time = (int) (Math.random() * 1000);
 
@@ -66,7 +131,7 @@ public class Parcovka extends Thread {
 
     public static void setParcingpoin(int parcingpoin) {
         Parcovka.parcingpoin = parcingpoin;
-    }*/
+    }
 
 }
-
+*/
--- src/PotokiBlinov339/ParcovkaRun.java
+++ src/PotokiBlinov339/ParcovkaRun.java
@@ -5,9 +5,30 @@ import java.util.ArrayList;
 
 public class ParcovkaRun {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Parcovka("Car " + (i + 1)).start();
+            try {
+                Thread.sleep((int) Math.random() * 1000); //интервал 1-2 сек
+            } catch (InterruptedException e) {
+                System.out.println(e); //ошибка
+            }
+
+        }
+
+    }
+}
 
 
-        ArrayList<Parcovka> parking = new ArrayList<>();
+
+
+
+
+
+
+
+
+
+     /*   ArrayList<Parcovka> parking = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
             parking.add(new Parcovka(String.valueOf(i + 1)));
@@ -19,11 +40,6 @@ public class ParcovkaRun {
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }*/
-
-        }
-
-    }
-}
 
 
 
